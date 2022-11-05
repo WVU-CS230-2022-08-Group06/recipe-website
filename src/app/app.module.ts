7a0ec23d6dc4eed1942548530279402250c23411
lhaught01 <105867235+lhaught01@users.noreply.github.com>
2022-11-05 16:06:40 -0400

    Login/Signup
    Added login/signup routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { HeaderComponent } from './header/header.component';
+import { CarouselsComponent } from './carousels/carousels.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardsComponent } from './cards/cards.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { UserSignComponent } from './user-sign/user-sign.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CarouselsComponent,
+    FooterComponent,
+    CardsComponent,
+    TopNavBarComponent,
+    SearchBarComponent,
+    UserLoginComponent,
+    HomeLayoutComponent,
+    UserSignComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-sign/user-sign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-sign',
+  templateUrl: './user-sign.component.html',
+  styleUrls: ['./user-sign.component.css']
+})
+export class UserSignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
